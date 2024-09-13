8f12a93771ca9f26db0998005864f3913555e80b
GuiAlbqrqe <guialbqrqe@outlook.com>
2024-09-13 13:22:57 -0300

    Adding more possibilites of numbers to make the game harder.

--- secret_number.py
+++ secret_number.py
@@ -1,16 +1,27 @@
-# jodo da adivinhação
+# jodo do Número Secreto
 from random import randint
 
 print('**************************************')
-print('Seja bem vindo ao jogo de adivinhação!')
+print('Seja bem vindo ao jogo do Número Secreto!')
 print('**************************************')
 
 print('\n')
 
-numero_secreto = randint(1, 5)
+numero_secreto = randint(1, 101)
 numero_escolhido = 0
 
-while numero_secreto != int(numero_escolhido):
-    numero_escolhido = input('Escolha um número de 1 a 5: ')
+while True:
+    try:
+        numero_escolhido = int(input('Escolha um número de 1 a 100: '))
+    except:
+        print('Você escolheu um número invalido!')
+    else:
+        if numero_escolhido not in range(1, 101):
+            print('Número precisa estar entre 1 a 100!')
+            continue
+        elif numero_escolhido == numero_secreto:
+            print(f"Parabéns! o numero secreto é {numero_secreto}!")
 
-print(f'Parabéns! Você descobriu que o número secreto era o {numero_secreto}!')
+            break
+        else:
+            print("Você errou!")
